7d1147166ad727f6e44783a8ea1956e5c7e8dc6a
thehumanflag <bennett.sean@gmail.com>
2011-10-15 16:54:54 -0500

    index of first item in list is as expected

---
+++ test/unit/BddCloud.UnitTestingPatterns.Test/Sample00.GivenWhenThenPattern/When_list_of_string_with_ilist_contract_reads_first_index.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+using MavenThought.Commons.Testing;
+using SharpTestsEx;
+
+namespace BddCloud.UnitTestPatterns.Test.Sample00.GivenWhenThenPattern
+{
+    public class When_list_of_string_with_ilist_contract_reads_first_index
+        : ListOfStringSpecificationWithIListContract
+    {
+        private string _actual;
+
+        protected override void WhenIRun()
+        {
+            _actual = Sut[0];
+        }
+
+        [It]
+        public void Should_contain_first_item_in_list()
+        {
+            _actual.Should().Be.EqualTo(InitialItems.First());
+        }
+    }
+}
