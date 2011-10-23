1dfebb76d682b89dd3b355a87a07398fc99960d3
thehumanflag <bennett.sean@gmail.com>
2011-10-23 05:01:07 -0500

    renamed scenarios

---
+++ test/unit/BddCloud.UnitTestingPatterns.Test/Sample00.GivenWhenThenPattern/When_list_of_string_with_ilist_contract_has_item_removed.cs
@@ -0,0 +1,44 @@
+﻿using System.Linq;
+using MavenThought.Commons.Extensions;
+using Enumerable = MavenThought.Commons.Extensions.Enumerable;
+using MavenThought.Commons.Testing;
+using SharpTestsEx;
+
+namespace BddCloud.UnitTestPatterns.Test.Sample00.GivenWhenThenPattern
+{
+
+
+public class When_list_of_string_with_ilist_contract_has_item_removed 
+    : ListOfStringSpecificationWithIListContract
+{
+    protected override void GivenThat()
+    {
+        base.GivenThat();
+
+        this.InitialItems = Enumerable.Create("first", "second");
+    }
+
+    protected override void WhenIRun()
+    {
+        Sut.RemoveAt(1);
+    }
+
+    [It]
+    public void Should_have_count_of_one()
+    {
+        Sut.Count.Should().Be(1);
+    }
+
+    [It]
+    public void Should_have_first_item()
+    {
+        Sut.Contains("first").Should().Be.True();
+    }
+
+    [It]
+    public void Should_not_have_second_item()
+    {
+        Sut.Contains("second").Should().Be.False();
+    }
+}
+}
--- test/unit/BddCloud.UnitTestingPatterns.Test/Sample00.GivenWhenThenPattern/When_list_of_string_with_ilist_contract_has_item_removed_at_index_that_does_not_exist.cs
+++ test/unit/BddCloud.UnitTestingPatterns.Test/Sample00.GivenWhenThenPattern/When_list_of_string_with_ilist_contract_has_item_removed_at_index_that_does_not_exist.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using MavenThought.Commons.Extensions;
 using MavenThought.Commons.Testing;
 using SharpTestsEx;
 
 namespace BddCloud.UnitTestPatterns.Test.Sample00.GivenWhenThenPattern
 {
     [ExceptionSpecification]
-    public class When_list_of_string_with_ilist_contract_has_item_removed_at_index_that_does_not_exist : ListOfStringSpecificationWithIListContract
+    public class When_list_of_string_with_ilist_contract_has_item_removed_that_does_not_exist : ListOfStringSpecificationWithIListContract
     {
         private ArgumentOutOfRangeException _expectedException;
 
@@ -14,17 +15,13 @@ namespace BddCloud.UnitTestPatterns.Test.Sample00.GivenWhenThenPattern
         {
             base.GivenThat();
 
+            InitialItems = Enumerable.Create("first", "second");
             _expectedException = new ArgumentOutOfRangeException("index", "Index was out of range. Must be non-negative and less than the size of the collection.");
-        }
-
-        protected override IList<string> CreateSut()
-        {
-            return new List<string>();
         }
 
         protected override void WhenIRun()
         {
-            Sut.RemoveAt(0);
+            Sut.RemoveAt(2);
         }
 
         [It]
@@ -37,12 +34,6 @@ namespace BddCloud.UnitTestPatterns.Test.Sample00.GivenWhenThenPattern
         public void Should_have_expected_exception_message()
         {
             ExceptionThrown.Message.Should().Be.EqualTo(_expectedException.Message);
-        }
-
-        [It]
-        public void Should_have_expected_inner_exception()
-        {
-            ExceptionThrown.InnerException.Should().Be(_expectedException.InnerException);
-        }
+        }  
     }
 }
